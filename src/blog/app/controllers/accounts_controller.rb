aa7a7dcc258dedf28ba2529a3aa6337e3fa44c84
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-05-23 00:03:14 +0900

    Create signup function

--- src/blog/app/controllers/accounts_controller.rb
+++ src/blog/app/controllers/accounts_controller.rb
@@ -1,2 +1,15 @@
 class AccountsController < ApplicationController
+	def signup
+		if request.get? then
+			@user = User.new()
+			render "signup"
+		elsif request.post? then
+			@user = User.new(user_params_with_confirmation)
+			if @user.valid? then
+				@user.save
+				set_current_user_to_cookie(@user)
+				redirect_to controller: "accounts", action: "detail", username: @user.username
+			end
+		end
+	end
 end
