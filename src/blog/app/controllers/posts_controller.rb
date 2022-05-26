9884bdee4128d22e7f41c7c6ee825dac66fab05a
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-05-27 00:01:16 +0900

    Create new and create function

--- src/blog/app/controllers/posts_controller.rb
+++ src/blog/app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+	def new
+		@post = Post.new()
+	end
+
+	
+	def create
+		user_id = cookies.signed[:user_id]
+		post = Post.new(
+			title: params[:post][:title],
+			content: params[:post][:content]
+		)
+		post.user_id = user_id
+		if post.valid? then
+			post.save()
+		end
+	end
 end
